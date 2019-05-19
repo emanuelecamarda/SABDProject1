b2d469667c49fcef23bfb6ecae60d3dcf5cbb96a
emanuele <emanuelecamarda@gmail.com>
2019-05-19 19:44:37 +0200

    Add Main TODO// test all on docker enviroment and check persistence of output

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import query1.MainQuery1;
+import query3.MainQuery3;
+
+public class Main {
+
+    public static void main (String[] args) {
+        MainQuery1.main(null);
+        MainQuery3.main(null);
+    }
+}
